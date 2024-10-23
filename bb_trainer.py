1eace8be8c602b321476b2e44e1054d898fac602
Berta Sánchez Jalet <berta.sanchez.jalet@intel.com>
2024-10-23 15:43:09 -0600

    Update bb_trainer.py

--- bb_trainer.py
+++ bb_trainer.py
@@ -25,17 +25,19 @@ def get_random_num(min,max):
    num = random.randint(min,max)
    return num
 
-def get_recipe(method,process,ratio,grams):
+def get_ai_recipe(method,process,ratio,grams):
   
 
-    prompt = f'''Give me a great {method} coffee recipe for {grams} grams of a {process} with a ratio of 1:{ratio}. 
-            Suggest the best grind size for these characteristics and explain why. 
-            Also tell me how many clicks in the comandante c40 the suggested grind size will be.
-            Suggest the best pouring schedule for these characteristics and explain why.
-            Don't give me tips or notes.
-            '''
-    if method == 'Aeropress':
-        prompt = f'{prompt} For Aeropress take into account its maximum capacity of 200 ml, if I need to brew more than this suggest a recipe with bypass.' 
+    # prompt = f'''Give me a great {method} coffee recipe for {grams} grams of a {process} with a ratio of 1:{ratio}. 
+    #         Suggest the best grind size for these characteristics and explain why. 
+    #         Also tell me how many clicks in the comandante c40 the suggested grind size will be.
+    #         Suggest the best pouring schedule for these characteristics and explain why.
+    #         Don't give me tips or notes.
+    #         '''
+    # if method == 'Aeropress':
+    #     prompt = f'{prompt} For Aeropress take into account its maximum capacity of 200 ml, if I need to brew more than this suggest a recipe with bypass.' 
+
+    prompt = f'''Suggest a grind size in Comandante C40 clicks for a {method} {process} coffee with a ratio of 1:{ratio}'''
 
     load_dotenv()
 
@@ -48,7 +50,7 @@ def get_recipe(method,process,ratio,grams):
         messages=[
             {
                 "role": "system",
-                "content": "You are a coffee expert assistant. Provide the best recipe for the characteristics inputed."
+                "content": "You are a coffee expert assistant."
             },
             {
                 "role": "user",
@@ -63,9 +65,31 @@ def get_recipe(method,process,ratio,grams):
 
     return chat_completion.choices[0].message.content
 
+def get_recipe(method,process,ratio,grams):
+    
+    total_water = ratio*grams
+    bloom = 3*grams
+
+    pours = []
+
+    pours.append(bloom)
+    pours.append((total_water*0.4))
+
+    if process in ('Red Catuai Natural', 'Marsellesa Termico'):
+        for _ in range(2):
+            halfs = (total_water*0.6)/2
+            pours.append(pours[-1]+halfs)
+
+    elif process in ('Geisha Red Honey', 'Pacamara Honey'):
+        for _ in range(3):
+            thirds = (total_water*0.6)/3
+            pours.append(pours[-1]+thirds)
+
+    return pours
+
 def run():
-    methods = ['Aeropress','V60','Origami w/ Flat Filter','Origami w/ Cone Filter','Kalita','Plinc','Tornado','Graycano']
-    process = ['Slow Natural', 'Honey', 'Red Honey', 'Washed']
+    methods = ['Aeropress','V60','Origami w/ Flat Filter','Origami w/ Cone Filter','Kalita']
+    process = ['Red Catuai Natural', 'Marsellesa Termico', 'Geisha Red Honey', 'Pacamara Honey']
 
 
     print('Bloom Battles Trainer!')
@@ -73,8 +97,10 @@ def run():
 
     method = choose_option(methods)
     print('\n')
+
     process = choose_option(process)
     print('\n')
+
     grams = get_random_num(12,30)
     ratio = get_random_num(10,20)
 
@@ -83,11 +109,17 @@ def run():
     print(process)
     print(f'{grams}g')
     print(f'1:{ratio}')
-
     print('\n')
+
+    pours = get_recipe(method,process,ratio,grams)
+    print('Recipe:')
+    print(pours)
+    print('\n')
+
+
     choice  = input('Suggest a recipe? (y/n): ')
     if choice == 'y':
-        recipe = get_recipe(method,process,ratio,grams)
+        recipe = get_ai_recipe(method,process,ratio,grams)
         print(recipe)
         print('Get Bloomin!')
     else:
